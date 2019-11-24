8b6e55bb92ca2d3a806268edaf309c2b10a33c05
bruno <bvillasen@gmail.com>
2019-11-23 16:23:59 -0800

    updated matplotlib call

--- analysis/polytropic_star.py
+++ analysis/polytropic_star.py
@@ -1,9 +1,7 @@
 import sys, os
 import numpy as np
-import matplotlib as mpl
-if os.environ.get('DISPLAY','') == '':
-    print('no display found. Using non-interactive Agg backend')
-    mpl.use('Agg')
+import matplotlib 
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import h5py as h5
 
@@ -18,8 +16,8 @@ from load_data_cholla import load_snapshot_cholla
 from tools import create_directory
 
 dataDir = '/data/groups/comp-astro/bruno/stars/polytrope/'
-inputDir = dataDir + 'output_files/grav_0/'
-outputDir = figuresDirectory + 'grav_0./'
+inputDir = dataDir + 'output_files/grav_0.5/'
+outputDir = figuresDirectory + 'grav_0.5/'
 create_directory( outputDir )
 
 n_poly = 1.5
